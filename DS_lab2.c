ee96b6f9be027ac17a40019034cfa1f66f40d86b
riyag-cs24 <riyagupta.cs24@bmsce.ac.in>
2025-11-04 09:10:51 +0530

    Add files via upload

---
+++ DS_lab2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+// Stack operations
+void push(char ch) {
+    stack[++top] = ch;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+// Precedence of operators
+int precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-': return 1;
+        case '*':
+        case '/': return 2;
+        default: return 0;
+    }
+}
+
+// Convert infix to postfix
+void infixToPostfix(char* infix, char* postfix) {
+    int i = 0, j = 0;
+    char ch;
+
+    while ((ch = infix[i++]) != '\0') {
+        if (isalnum(ch)) {
+            postfix[j++] = ch;
+        } else if (ch == '(') {
+            push(ch);
+        } else if (ch == ')') {
+            while (peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); // Remove '('
+        } else {
+            while (precedence(peek()) >= precedence(ch)) {
+                postfix[j++] = pop();
+            }
+            push(ch);
+        }
+    }
+
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[MAX], postfix[MAX];
+
+    printf("Enter a valid parenthesized infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
